f7c94ee6f1c7a34027bea1589c5b7dbf1be9e990
Tim Schagen <schagen34@gmail.com>
2018-03-01 17:16:35 +0100

    Slechte multiplayer toegevoegd

---
+++ Day of the Dead/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private const float movementSpeed = 0.3f;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+            this.transform.position += new Vector3(0, 0, movementSpeed);
+
+        if (Input.GetKey(KeyCode.S))
+            this.transform.position -= new Vector3(0, 0, movementSpeed);
+
+        if (Input.GetKey(KeyCode.A))
+            this.transform.position -= new Vector3(movementSpeed, 0);
+        if (Input.GetKey(KeyCode.D))
+            this.transform.position += new Vector3(movementSpeed, 0);
+	}
+}
