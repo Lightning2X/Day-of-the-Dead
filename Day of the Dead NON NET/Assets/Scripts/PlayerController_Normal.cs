296da8b865fb3eedf0137b884a6666e5fa392a95
Tim "Lightning2x" Schagen <schagen34@gmail.com>
2018-03-01 21:37:46 +0100

    added non network space to test out stuff

---
+++ Day of the Dead NON NET/Assets/Scripts/PlayerController_Normal.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_Normal : MonoBehaviour {
+    private float movementSpeed = 0.3f;
+    private float sprintAmplify = 1.5F;
+    private float jumpHeight = 260F;
+    private float rotationSpeed = 100F;
+    private float cameraDistance = 15f;
+    private float cameraHeight = 15f;
+    Rigidbody rb;
+    Transform mainCamera;
+    Vector3 cameraOffset;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        cameraOffset = new Vector3(0f, cameraHeight, -cameraDistance);
+        mainCamera = Camera.main.transform;
+        CameraMover();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool sprint = false;
+        if (Input.GetKey(KeyCode.LeftShift))
+            sprint = true;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(sprint)
+                this.transform.position += transform.forward * sprintAmplify * movementSpeed;
+            else
+                this.transform.position += transform.forward * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (sprint)
+                this.transform.position += transform.forward * sprintAmplify * movementSpeed;
+            else
+                this.transform.position += transform.forward * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+            this.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+        if (Input.GetKey(KeyCode.D))
+            this.transform.Rotate(-Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+        if (Input.GetKey(KeyCode.Space))
+            transform.Translate(Vector3.up * jumpHeight * Time.deltaTime, Space.World);
+
+        CameraMover();
+    }
+
+    void CameraMover()
+    {
+        mainCamera.position = this.transform.position;
+        mainCamera.rotation = this.transform.rotation;
+        mainCamera.Translate(cameraOffset);
+        mainCamera.LookAt(transform);
+    }
+}
--- Day of the Dead/Assets/Scripts/PlayerController_Net.cs
+++ Day of the Dead/Assets/Scripts/PlayerController_Net.cs
@@ -5,8 +5,12 @@ using UnityEngine.Networking;
 
 public class PlayerController_Net : NetworkBehaviour {
     private float movementSpeed = 0.3f;
+    private float sprintAmplify = 1.5F;
+    private float jumpHeight = 260F;
+    private float rotationSpeed = 100F;
     private float cameraDistance = 15f;
     private float cameraHeight = 15f;
+    Rigidbody rb;
     Transform mainCamera;
     Vector3 cameraOffset;
     // Use this for initialization
@@ -16,6 +20,7 @@ public class PlayerController_Net : NetworkBehaviour {
             Destroy(this);
             return;
         }
+        rb = GetComponent<Rigidbody>();
         cameraOffset = new Vector3(0f, cameraHeight, -cameraDistance);
         mainCamera = Camera.main.transform;
         CameraMover();
@@ -23,14 +28,30 @@ public class PlayerController_Net : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        bool sprint = false;
+        if (Input.GetKey(KeyCode.LeftShift))
+            sprint = true;
+
         if (Input.GetKey(KeyCode.W))
-            this.transform.position += transform.forward * movementSpeed;
+        {
+            if(sprint)
+                this.transform.position += transform.forward * sprintAmplify * movementSpeed;
+            else
+                this.transform.position += transform.forward * movementSpeed;
+        }
         if (Input.GetKey(KeyCode.S))
-            this.transform.position -= transform.forward * movementSpeed;
+        {
+            if (sprint)
+                this.transform.position += transform.forward * sprintAmplify * movementSpeed;
+            else
+                this.transform.position += transform.forward * movementSpeed;
+        }
         if (Input.GetKey(KeyCode.A))
-            this.transform.Rotate(Vector3.up * Time.deltaTime * 100f);
+            this.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
         if (Input.GetKey(KeyCode.D))
-            this.transform.Rotate(-Vector3.up * Time.deltaTime * 100f);
+            this.transform.Rotate(-Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+        if (Input.GetKey(KeyCode.Space))
+            transform.Translate(Vector3.up * jumpHeight * Time.deltaTime, Space.World);
 
         CameraMover();
     }
