dbc089973e5b91a813264d7312b4bdf3bc20c5f6
Tim Schagen <schagen34@gmail.com>
2018-03-02 12:36:35 +0100

    added jumping

--- Day of the Dead/Assets/Scripts/PlayerController_Net.cs
+++ Day of the Dead/Assets/Scripts/PlayerController_Net.cs
@@ -6,13 +6,15 @@ using UnityEngine.Networking;
 public class PlayerController_Net : NetworkBehaviour {
     private float movementSpeed = 0.3f;
     private float sprintAmplify = 1.5F;
-    private float jumpHeight = 50F;
+    private float jumpSpeed = 15F;
     private float rotationSpeed = 100F;
     private float cameraDistance = 15f;
     private float cameraHeight = 15f;
     private CharacterController controller;
     Transform mainCamera;
     Vector3 cameraOffset;
+    Vector3 moveDirection = Vector3.zero;
+    float gravity = -Physics.gravity.y;
     // Use this for initialization
     void Start()
     {
@@ -52,7 +54,10 @@ public class PlayerController_Net : NetworkBehaviour {
             this.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
         if (Input.GetKey(KeyCode.D))
             this.transform.Rotate(-Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
-
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+            moveDirection.y = jumpSpeed;
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
         CameraMover();
     }
 
