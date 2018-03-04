b636fa781e0704fc747992408ebdbefb255b7ee9
Jacco van Mourik <jacco.vanmourik@me.com>
2018-03-04 22:05:42 +0100

    Add playerController, Weapon, CharacterProperties
    Simple melee weapon
    Player movement
    Player sprint
    Player jump
    Player camera control
    Camera aiming
    Weapon class (damage, spread and attack range)
    Character properties class (health and isAlive)

---
+++ Day of the Dead NON NET/Assets/Scripts/CharacterProperties.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterProperties : MonoBehaviour {
+	public bool isAlive = true;
+	public int _health = 1;
+	public int health {
+		get {
+			return _health;
+		}
+		set {
+			_health = value;
+			if (value <= 0)
+				isAlive = false;
+			else
+				isAlive = true;
+		}
+	}
+}
---
+++ Day of the Dead NON NET/Assets/Scripts/WeaponProperties.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponProperties : MonoBehaviour {
+	public int damage = 1;
+	public float attackRange = 5f;
+	public float spread = 90;
+}
