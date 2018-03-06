9edb2eb1930bc6b2b385b838748e7358cdd2a6f8
Jacco van Mourik <jacco.vanmourik@me.com>
2018-03-06 20:59:45 +0100

    Map verwijderd

---
+++ Assets/Scripts/WeaponProperties.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponProperties : MonoBehaviour {
+	public bool isMelee = false;
+	public int damage = 1;
+	public float attackRange = 5f;
+	public float spread = 90;
+}
--- Day of the Dead NON NET/Assets/Scripts/WeaponProperties.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WeaponProperties : MonoBehaviour {
-	public int damage = 1;
-	public float attackRange = 5f;
-	public float spread = 90;
-}
