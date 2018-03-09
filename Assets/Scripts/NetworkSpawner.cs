8a7eb428f297124d6c2d5fb4992bb992db3ef33e
Jacco van Mourik <jacco.vanmourik@me.com>
2018-03-09 13:43:47 +0100

    Jacco -> Master

---
+++ Assets/Scripts/NetworkSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSpawner : NetworkBehaviour {
+
+	[SerializeField] GameObject spawnPrefab;
+	[SerializeField] int amount = 1;
+
+	public override void OnStartServer () {
+		for (int i = 0; i < amount; i++)
+		{
+			//Instantiate the prefab
+			GameObject gameObject = Instantiate(spawnPrefab);
+
+			//Spawn the GameObject you assigned in the Inspector
+			NetworkServer.Spawn(gameObject);
+		}
+	}
+}
