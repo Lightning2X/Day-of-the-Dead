85bed9fd5e828ff899887937bce13598ab7783e2
FreakyFrek <33694918+FreakyFrek@users.noreply.github.com>
2018-03-22 21:23:55 +0100

    Verschillende lichaamsdelen verschillende kleuren
    Staat allemaal in CharacterProperties

--- Assets/Scripts/CharacterProperties.cs
+++ Assets/Scripts/CharacterProperties.cs
@@ -6,11 +6,19 @@ using UnityEngine.Networking;
 public class CharacterProperties : NetworkBehaviour {
 	public bool isAlive = true;
 
-	[SyncVar]
+    [SerializeField] GameObject head;
+    [SerializeField] GameObject torso;
+    [SerializeField] GameObject legs;
+    Material material;
+
+    [SyncVar]
 	public int _health = 1;
 
-
-	public int health {
+    private void Start()
+    {
+ 
+    }
+    public int health {
 		get {
 			return _health;
 		}
@@ -39,4 +47,25 @@ public class CharacterProperties : NetworkBehaviour {
 		if (!isAlive)
 			NetworkServer.Destroy (gameObject);
 	}
+
+    public void SetColors(int[] colorIndexes)
+    {
+        SetObjectColor(head, colorIndexes[0]);
+        SetObjectColor(torso, colorIndexes[1]);
+        SetObjectColor(legs, colorIndexes[2]);
+    }
+
+    private void SetObjectColor(GameObject gameObject, int i)
+    {
+        material = gameObject.GetComponent<Renderer>().material;
+        if (i == 0)
+            material.color = new Color(0.690F, 0.125F, 0.082F); //Rood
+        else if (i == 1)
+            material.color = new Color(0.749F, 0.420F, 0F); //Oranje
+        else if (i == 2)
+            material.color = new Color(0.071F, 0.534F, 0.722F); //Blauw
+        else if (i == 3)
+            material.color = new Color(0.392F, 0.690F, 0.278F); //Groen
+    }
+
 }
